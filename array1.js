c50ca77b19550abe600cb89c4aafa96203541b54
mahfuz <mdmahfuzbd@gmail.com>
2021-01-16 23:48:51 +0600

    array1.js added

---
+++ array1.js
@@ -0,0 +1,7 @@
+var arr = [2,3,4];
+// arr.push(6);
+// arr.pop();
+arr.shift();
+arr.unshift(8);
+console.log(arr.length)
+console.log(arr)
---
+++ hello.js
@@ -0,0 +1,13 @@
+var price = 121;
+console.log(price);
+console.log(typeof price);
+
+var firstName = "kalim";
+console.log(typeof firstName);
+
+var isHot = true;
+console.log(typeof isHot);
+var n1 = "15.3"
+n1 = n1;
+console.log(n1);
+console.log(Math.random()*10);
