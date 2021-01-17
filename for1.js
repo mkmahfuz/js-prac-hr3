7419866653218c471f3158e698ebf1cff324ff63
mahfuz <mdmahfuzbd@gmail.com>
2021-01-17 21:12:59 +0600

    for1.js added

---
+++ for1.js
@@ -0,0 +1,20 @@
+for(var i=0;i<10;i++){
+    console.log(i);
+}
+
+// for(x in object)
+var obj={name:"khalid",phone:0123,key:"value"};
+for(x in obj){
+    console.log(x);
+    }
+for(x in obj){
+    // console.log(obj[x]);
+    console.log(`${x} = ${obj[x]}`);
+    }
+ //for(x of array)
+ var arr=[1,2,3,4,5]   
+ for(x of arr){
+     console.log(x);
+ }
+
+ 
