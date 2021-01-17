a7c08b1b09a2bf90883a17b6fc56d8511830265c
mahfuz <mdmahfuzbd@gmail.com>
2021-01-17 21:01:37 +0600

    while1.js added

---
+++ while1.js
@@ -0,0 +1,14 @@
+var num = 0;
+while(num<10){
+    // console.log(num);
+    // num = num + 1; //this is same as num++
+    num++;
+}
+
+for(var i=0;i<10;i++){
+    console.log(i);
+}
+
+var n1 = [1,2,3,4,5];
+var result = n1.slice(-1,3);
+console.log(result);
