ced0010d81d451e4a2816c7411c71870b7f49059
mahfuz <mdmahfuzbd@gmail.com>
2021-01-20 23:15:54 +0600

    recursive factorial added

--- closures1.js
+++ closures1.js
@@ -14,7 +14,7 @@ var add1 = function(){counter1 += 1; return counter1};
 console.log(add1(),add1(),add1());
 
 //function self invoked and use it as closures
-
+// 1st self invoked function which return a function expression.
 var add2 = (function(){ var counter2 = 0; return function(){counter2 += 1; return counter2}})();
 
 console.log(add2(),add2(),add2(),add2());
---
+++ recursivefact.js
@@ -0,0 +1,11 @@
+//recursive way factorial
+
+function factorial(n){
+    if(n == 0){
+        return 1;
+    }else{
+      return n * factorial(n-1);
+    }
+}
+
+console.log(factorial(10)); //3628800
