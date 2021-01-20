5d41303d2d2f5ac19c7f8c1d7ffdebb8b8033321
mahfuz <mdmahfuzbd@gmail.com>
2021-01-20 20:48:42 +0600

    closures1.js added

---
+++ closures1.js
@@ -0,0 +1,20 @@
+function add(){
+    var counter = 0;
+    function plus(){counter += 1;}
+    plus();
+    return counter;
+}
+
+console.log(add(),add());
+
+//function expression
+var counter1 = 0 ;
+var add1 = function(){counter1 += 1; return counter1};
+
+console.log(add1(),add1(),add1());
+
+//function self invoked and use it as closures
+
+var add2 = (function(){ var counter2 = 0; return function(){counter2 += 1; return counter2}})();
+
+console.log(add2(),add2(),add2(),add2());
