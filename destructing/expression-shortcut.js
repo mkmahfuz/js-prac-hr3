bf4f65e4bc74936d69289e97cb84be854ace4149
mahfuz <mdmahfuzbd@gmail.com>
2021-02-16 20:20:07 +0600

    string to number conversion short-cut added

--- destructing/expression-shortcut.js
+++ destructing/expression-shortcut.js
@@ -27,6 +27,36 @@ let y1 = active || 6;
 console.log(x1) // 5 ---when active is true but false --when active is false
 console.log(y1) // true --when active is true but 6 --when active is true
 
+//converting string to number
+const numx1 = "45";
+console.log(typeof(numx1)) //string
+
+const numx2 = +"46";
+console.log(typeof(numx2)) //number
+
+let numx3 = "5";
+console.log(typeof(+numx3)) //number
+numx3 = +numx3;
+console.log(typeof(numx3)) //number
+//but
+let numx4 = "6";
+numx4 +=numx4;
+console.log(typeof(numx4)) //string
+
+//converting number to string
+
+const strx1 = 5 + "";
+console.log(typeof(strx1)) //string
+
+let strx2 = 6;
+console.log(typeof(strx2)) //number
+
+strx2 += "";
+console.log(typeof(strx2)) //string
+
+const strx3 = "" + 5;
+console.log(typeof(strx3)) //string
+
 
 
 // ?.
